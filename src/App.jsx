d962942516302e3dd2d1a8a032c17d59de952c9c
Nikko <yuzkonikita@gmail.com>
2024-09-25 11:17:43 +0200

    two files were created and connected index.scss and _base.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import "./sass/index.scss"
 import Footer from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 import Main from './components/Main/Main'
